d2a04dff0e967d7a1f432ccac5fdd49076550553
Maithri CH <maithrich3@gmail.com>
2020-09-11 16:51:53 -0700

    Revert "del all"
    This reverts commit 97bbaae3f90f86bdb9cbe3a81eb01cd418f25b28.

---
+++ precache-manifest.86e15007af8a7293c84e2ddaa43a0c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce285be90a50149ff0431b3a69c882c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62432ae0da23361880d8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a17bf967ac26a8d3feeb",
+    "url": "/static/js/2.5f887cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f887cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62432ae0da23361880d8",
+    "url": "/static/js/main.c81c8501.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
